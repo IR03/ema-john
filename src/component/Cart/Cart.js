49e6d9abbd93ec05f0b3c1392f918b639a790c48
IR03 <iftikharsakib@gmail.com>
2021-04-13 22:45:30 +0600

    Finally DOne

---
+++ src/component/Cart/Cart.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    // const total = cart.reduce((total, prd)=> total+ prd.price, 0)
+    let total = 0;
+    for(let i=0; i<cart.length; i++){
+        const product = cart[i];
+        total = total + product.price;
+    }
+
+    let shipping = 0;
+    if(total>35){
+        shipping = 0;
+    }
+    else if(total>15){
+        shipping = 4.99;
+    }
+    else if(total > 0){
+        shipping = 4.99
+    }
+    const tax =total/10;
+    const grandTotal =Math.round(total+shipping+tax);
+    
+    const formatNumber = num=> {
+        const precision = Math.round(num);
+        return Number(precision);
+    }
+    return (
+        <div>
+            <h4>Order Summary</h4>
+            <p>Items Ordered : {cart.length}</p>
+            <p>Product Price : {formatNumber(total)}</p>
+            <p><small>Shipping Cost : {formatNumber(shipping)}</small></p>
+            <p><small>Tax + VAT : {formatNumber(tax)}</small></p>
+            <p>Total Price : {formatNumber(grandTotal)}</p>
+        </div>
+    );
+};
+
+export default Cart;
--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css'
 
-const Product = (props) => {
-    // console.log(props.product);
-
+    const Product = (props) => {
+     //console.log(props.handleAddProduct);
+     
+    const name = props.product?.name;
+    const img = props.product?.img;
+    const seller = props.product?.seller;
+    const price = props.product?.price;
+    const stock = props.product?.stock;
     return (
         <div className="product">
-            <h4>{props.product?.name}</h4>
-            <h4>{props.product?.wholePrice}</h4>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <br />
+                <p><small>by: {seller}</small></p>
+                <p><small>${price}</small></p>
+                <p><small>Only {stock} left in stock - order soon</small></p>
+                <button 
+                   className="main-button" 
+                   onClick={()=>props.handleAddProduct(props.product)}
+                >
+                    <FontAwesomeIcon icon={faShoppingCart} />add to cart
+                </button>
+            </div>
         </div>
-        
+
     );
 };
 
